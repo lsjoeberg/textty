a9a8f1d3a4fa018bb0c4dddfb5c0d6ed6f40d12c
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-03-08 15:22:50 +0100

    Implement fg and bg colour parsing from html

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,134 @@
 use ansi_term::Colour;
 use scraper::{Html, Selector};
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug)]
+struct PageStyle {
+    bg: BgColour,
+    fg: FgColour,
+}
+
+impl Default for PageStyle {
+    fn default() -> Self {
+        Self {
+            bg: BgColour::Black,
+            fg: FgColour::White,
+        }
+    }
+}
+
+impl FromStr for PageStyle {
+    type Err = String;
+
+    // Parse colour codes from space-separated fields, e.g. 'bgB W bgImg'.
+    // There can be 1-3 fields present in the string.
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(' ').collect::<Vec<&str>>();
+        match parts[..] {
+            [s0] => {
+                let bg = s0.parse()?;
+                Ok(Self {
+                    bg,
+                    fg: FgColour::default(),
+                })
+            }
+            [s0, s1] => {
+                let bg = s0.parse()?;
+                let fg = s1.parse()?;
+                Ok(Self { bg, fg })
+            }
+            // TODO: Converting referenced GIF from 'bgImg' to ASCII is not yet supported.
+            [_, _, _] => Ok(Self::default()),
+            _ => Err(format!("invalid svt colour class: {s}")),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum BgColour {
+    Black,
+    Blue,
+    White,
+    Yellow,
+}
+
+impl Default for BgColour {
+    fn default() -> Self {
+        Self::Black
+    }
+}
+
+impl FromStr for BgColour {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bg = match s {
+            "bgB" => Self::Blue,
+            "bgBl" => Self::Black,
+            "bgW" => Self::White,
+            "bgY" => Self::Yellow,
+            _ => return Err(format!("invalid bg: {s}")),
+        };
+        Ok(bg)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum FgColour {
+    Black,
+    Blue,
+    Cyan,
+    Green,
+    Red,
+    White,
+    Yellow,
+}
+
+impl Default for FgColour {
+    fn default() -> Self {
+        Self::White
+    }
+}
+
+impl FromStr for FgColour {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let fg = match s {
+            "bl" => Self::Black,
+            "B" => Self::Blue,
+            "C" => Self::Cyan,
+            "G" => Self::Green,
+            "R" => Self::Red,
+            "W" => Self::White,
+            "Y" => Self::Yellow,
+            _ => return Err(format!("invalid fg: {s}")),
+        };
+        Ok(fg)
+    }
+}
+
+impl From<PageStyle> for ansi_term::Style {
+    fn from(value: PageStyle) -> Self {
+        let style = Self::new();
+        // Set background
+        let style = match value.bg {
+            BgColour::Black => style.on(Colour::Black),
+            BgColour::Blue => style.on(Colour::Blue),
+            BgColour::White => style.on(Colour::White),
+            BgColour::Yellow => style.on(Colour::Yellow),
+        };
+        // Set foreground
+        match value.fg {
+            FgColour::Black => style.fg(Colour::Black),
+            FgColour::Blue => style.fg(Colour::Blue),
+            FgColour::Cyan => style.fg(Colour::Cyan),
+            FgColour::Green => style.fg(Colour::Green),
+            FgColour::Red => style.fg(Colour::Red),
+            FgColour::White => style.fg(Colour::White),
+            FgColour::Yellow => style.fg(Colour::Yellow),
+        }
+    }
+}
 
 pub fn parse(html: &str) -> String {
     let fragment = Html::parse_fragment(html);
@@ -9,6 +138,7 @@ pub fn parse(html: &str) -> String {
     // `line`, and `line DH`. Use a wildcard to match all line variants.
     let selector = Selector::parse(r#"span[class*="line"]"#).unwrap();
 
+    // TODO: Preallocate string with some reasonable size.
     let mut page = String::new();
 
     for element in fragment.select(&selector) {
@@ -17,14 +147,9 @@ pub fn parse(html: &str) -> String {
             let class_str = c.attr("class").unwrap();
             let text = c.text().collect::<String>();
 
-            let text_coloured = match class_str.chars().last().unwrap() {
-                'Y' => Colour::Yellow.paint(&text).to_string(),
-                'C' => Colour::Cyan.paint(&text).to_string(),
-                'B' => Colour::Blue.paint(&text).to_string(),
-                'G' => Colour::Green.paint(&text).to_string(),
-                _ => text,
-            };
-            page.push_str(&text_coloured.to_string());
+            let svt_color = PageStyle::from_str(class_str).unwrap();
+            let style = ansi_term::Style::from(svt_color);
+            page.push_str(&style.paint(&text).to_string());
         }
         page.push('\n');
     }
@@ -33,4 +158,42 @@ pub fn parse(html: &str) -> String {
     page.pop(); // Remove last '\n'
 
     page
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase {
+        input: &'static str,
+        expected: PageStyle,
+    }
+
+    #[test]
+    fn test_parse_page_style() {
+        let test_cases = [
+            TestCase {
+                input: "bgB",
+                expected: PageStyle {
+                    bg: BgColour::Blue,
+                    fg: FgColour::White,
+                },
+            },
+            TestCase {
+                input: "bgBl W",
+                expected: PageStyle {
+                    bg: BgColour::Black,
+                    fg: FgColour::White,
+                },
+            },
+            TestCase {
+                input: "bgB W bgImg",
+                expected: PageStyle::default(),
+            },
+        ];
+        for case in test_cases.iter() {
+            let result = PageStyle::from_str(case.input).unwrap();
+            assert_eq!(result, case.expected);
+        }
+    }
 }
